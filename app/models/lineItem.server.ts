0a82d204c91f78c2a5174ca2d5c8500b0e2fd252
Varenya Thyagaraj <varen90@gmail.com>
2022-03-20 14:36:14 +0530

    add line item to db via form

--- app/models/lineItem.server.ts
+++ app/models/lineItem.server.ts
@@ -1,7 +1,15 @@
 import { prisma } from "~/db.server";
 
+import { LineItem } from "@prisma/client";
+
 export function getLineItems({ estimateId }: { estimateId: string }) {
   return prisma.lineItem.findMany({
     where: { estimateId },
+  });
+}
+
+export function createLineItem(lineItem: Omit<LineItem, "id">) {
+  return prisma.lineItem.create({
+    data: lineItem,
   });
 }
