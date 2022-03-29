8793ddd0355269101b95083453295a58c6dacdb2
Varenya Thyagaraj <varen90@gmail.com>
2022-03-29 23:21:14 +0530

    create a layout component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Project } from "@prisma/client";
+import React from "react";
 import {
   ActionFunction,
   json,
@@ -11,6 +12,10 @@ import invariant from "tiny-invariant";
 import { CreateProject } from "~/components/CreateProject";
 import { createProject, getProjects } from "~/models/project.server";
 import indexStyles from "~/styles/index_page.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: indexStyles }];
+}
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
@@ -25,59 +30,56 @@ export const action: ActionFunction = async ({ request }) => {
   return redirect("/");
 };
 
-export function links() {
-  return [{ rel: "stylesheet", href: indexStyles }];
-}
-
 export const loader: LoaderFunction = async () => {
   return json<Project[]>(await getProjects());
 };
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen">
+      <section className="bg-gray-800 py-12 text-white">
+        <header>
+          <h1 className="p-8 text-center text-4xl md:text-5xl">
+            Project Estimator
+          </h1>
+        </header>
+        <h2 className="mt-2 mb-4 px-5 text-center text-xl md:text-2xl">
+          Hey 👋🏻, seems like you don't have any projects. Lets get started!
+        </h2>
+      </section>
+      <div className="project-view flex items-center justify-center bg-gray-200">
+        <div className="project-creation-form mx-auto">{children}</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Index() {
   const projects = useLoaderData<Project[]>();
   if (projects.length === 0) {
     return (
-      <div className="min-h-screen">
-        <section className="bg-gray-800 py-12 text-white">
-          <header>
-            <h1 className="p-8 text-center text-4xl md:text-5xl">
-              Project Estimator
-            </h1>
-          </header>
-          <h2 className="mt-2 mb-4 px-5 text-center text-xl md:text-2xl">
-            Hey 👋🏻, seems like you don't have any projects. Lets get started!
-          </h2>
-        </section>
-        <div className="project-view flex items-center justify-center bg-gray-200">
-          <div className="project-creation-form mx-auto">
-            <CreateProject />
-          </div>
-        </div>
-      </div>
+      <Layout>
+        <CreateProject />
+      </Layout>
     );
   }
+
   return (
-    <div className="min-h-screen bg-gray-50">
-      <header>
-        <h1 className="p-8 text-center text-5xl">Project Estimator</h1>
-      </header>
-      <div className="project-view container mx-auto mt-16 rounded-lg bg-gray-200 p-8 shadow-lg">
-        <nav className="min-h-full w-64 p-4">
-          <ul>
-            {projects.map((project) => (
-              <li key={project.id} className="pb-4">
-                <Link
-                  to={`/project/${project.projectName}`}
-                  className="block text-left text-2xl hover:underline"
-                >
-                  {project.projectName}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <main></main>
-      </div>
-    </div>
+    <Layout>
+      <nav className="min-h-full w-64 p-4">
+        <ul>
+          {projects.map((project) => (
+            <li key={project.id} className="pb-4">
+              <Link
+                to={`/project/${project.projectName}`}
+                className="block text-left text-2xl hover:underline"
+              >
+                {project.projectName}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </Layout>
   );
 }
